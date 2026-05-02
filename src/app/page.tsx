8cdd5da0f9eb095bb9453e6ef94711832b6eb83e
Julia Sales <juliasalesc@outlook.com>
2026-05-02 18:11:26 -0300

    feat: chart-card, donut-chart and month-select components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Sidebar } from './_components/sidebar';
 import { Header } from './_components/header';
 import BalanceCard from './_components/balance-card';
 import { FinancialMetricCard } from './_components/financial-metric-card';
+import { ChartCard } from './_components/chart-card';
 
 export default function Home() {
     return (
@@ -23,8 +24,13 @@ export default function Home() {
                         <FinancialMetricCard />
                     </section>
 
-                    <section>
-                        
+                    <section className='grid grid-cols-1 gap-6 lg:grid-cols-2'>
+                        <ChartCard
+                            depositsTotal={1000}
+                            expensesTotal={500}
+                            investmentsTotal={500}
+                            balance={1000}
+                        />
                     </section>
                 </main>
             </div>
